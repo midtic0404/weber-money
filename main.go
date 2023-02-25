3a49a196395297008b8181b72dd81e467ba1e974
Cha Cha <midtic0404@gmail.com>
2023-02-24 21:35:45 -0700

    Improve logging and code structure

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	// Selectors for in state and out of state tuition
+	inStateSelector = "#Body > main > div > div > div > div.large-9.columns > table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2)"
+	outStateSelector = "#Body > main > div > div > div > div.large-9.columns > table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(3)"
 )
 
 func main() {
@@ -13,18 +20,18 @@ func main() {
 
 	res, err := http.Get("https://www.weber.edu/admissions/shared/costs.html")
 	if err != nil {
-		fmt.Println("Error: ", err)
+		log.Fatal("Error when getting the page: ", err)
 	}
 	defer res.Body.Close()
 
 	document, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		fmt.Println("Getting document failed: ")
+		log.Fatal("Error when creating document: ", err)
 	}
 
-	inState = document.Find("#Body > main > div > div > div > div.large-9.columns > table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2)").First().Text()
-
-	outState = document.Find("#Body > main > div > div > div > div.large-9.columns > table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(3)").First().Text()
+	// Scrape tuition for in state and out of state
+	inState = document.Find(inStateSelector).First().Text()
+	outState = document.Find(outStateSelector).First().Text()
 
 	fmt.Println("In state tuition: ", inState)
 	fmt.Println("Out of state tuition: ", outState)
